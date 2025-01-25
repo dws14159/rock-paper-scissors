fca455b0a366694128e1ad4b4766c70a60afa466
Dave <19504699+dws14159@users.noreply.github.com>
2025-01-25 02:25:25 +0000

    Add getHumanChoice

--- script.js
+++ script.js
@@ -7,4 +7,23 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let userInput = prompt("Enter rock, paper or scissors");
+
+    while (true) {
+
+        userInput = (userInput ?? "").toLowerCase();
+
+        switch (userInput) {
+            case "rock":
+            case "paper":
+            case "scissors":
+                return userInput;
+
+            default:
+                userInput = prompt(`Invalid choice: ${userInput}. Please enter rock, paper or scissors`);
+        }
+    }
+}
+
 console.log("Hello world");
